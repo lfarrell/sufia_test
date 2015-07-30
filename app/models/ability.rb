b391690292ac65fb227ff224a6523ff01f7e0078
lfarrell <lfarrell@email.unc.edu>
2015-07-30 13:19:15 -0400

    Add in path to fedora, solr & fits. Add admin abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,9 +2,12 @@ class Ability
   include Hydra::Ability
   include Sufia::Ability
 
-  
+   
   # Define any customized permissions here.
   def custom_permissions
+    if current_user.admin?
+      can [:create, :show, :add_user, :remove_user, :index], Role
+    end
     # Limits deleting objects to a the admin user
     #
     # if current_user.admin?
--- config/initializers/sufia.rb
+++ config/initializers/sufia.rb
@@ -98,7 +98,7 @@ Sufia.config do |config|
   # config.persistent_hostpath = 'http://localhost/files/'
 
   # If you have ffmpeg installed and want to transcode audio and video uncomment this line
-  # config.enable_ffmpeg = true
+  config.enable_ffmpeg = true
 
   # Sufia uses NOIDs for files and collections instead of Fedora UUIDs
   # where NOID = 10-character string and UUID = 32-character string w/ hyphens
@@ -111,7 +111,7 @@ Sufia.config do |config|
   # config.redis_namespace = "sufia"
 
   # Specify the path to the file characterization tool:
-  # config.fits_path = "fits.sh"
+  config.fits_path = "/usr/local/Cellar/fits/0.8.6-1/fits.sh"
 
   # Specify how many seconds back from the current time that we should show by default of the user's activity on the user's dashboard
   # config.activity_to_show_default_seconds_since_now = 24*60*60
